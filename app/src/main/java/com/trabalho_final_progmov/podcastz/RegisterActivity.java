7957fbd2ce71f16851cff88e826133abfc2133e2
Henrique <hp_bonfim@hotmail.com>
2023-06-25 09:29:39 -0400

    movendo o botao de voltar para o topo

--- app/src/main/java/com/trabalho_final_progmov/podcastz/RegisterActivity.java
+++ app/src/main/java/com/trabalho_final_progmov/podcastz/RegisterActivity.java
@@ -1,4 +1,5 @@
 package com.trabalho_final_progmov.podcastz;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -7,77 +8,129 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private FirebaseAuth auth;
-    private EditText inputFirstName, inputLastName, inputEmail, inputPassword, inputConfirmPassword;
-    private Button buttonRegister, buttonLogin;
+  private FirebaseAuth auth;
+  private EditText inputFirstName, inputLastName, inputEmail, inputPassword, inputConfirmPassword;
+  private Button buttonRegister;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_register);
 
-        // Iniciando Firebase Auth
-        auth = FirebaseAuth.getInstance();
+    // Iniciando Firebase Auth
+    auth = FirebaseAuth.getInstance();
 
-        inputFirstName = findViewById(R.id.input_first_name);
-        inputLastName = findViewById(R.id.input_last_name);
-        inputEmail = findViewById(R.id.input_email);
-        inputPassword = findViewById(R.id.input_password);
-        inputPassword = findViewById(R.id.input_password);
+    // Toolbar para o botão de voltar
+    Toolbar toolbar = findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        buttonRegister = findViewById(R.id.button_register);
-        buttonLogin = findViewById(R.id.button_return_login);
+    // Iniciando componentes
+    inputFirstName = findViewById(R.id.input_first_name);
+    inputLastName = findViewById(R.id.input_last_name);
+    inputEmail = findViewById(R.id.input_email);
+    inputPassword = findViewById(R.id.input_password);
+    inputPassword = findViewById(R.id.input_password);
+    buttonRegister = findViewById(R.id.button_register);
 
-        buttonRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = inputEmail.getText().toString().trim();
-                String password = inputPassword.getText().toString().trim();
+    // Botão de voltar
+    toolbar.setNavigationOnClickListener(
+      new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+          Intent intent = new Intent(
+            RegisterActivity.this,
+            LoginActivity.class
+          );
+          startActivity(intent);
+          finish();
+        }
+      }
+    );
 
-                if (TextUtils.isEmpty(email)) {
-                    Toast.makeText(getApplicationContext(), "Por favor, insira um email!", Toast.LENGTH_SHORT).show();
-                    return;
-                }
+    // Botão de registro
+    buttonRegister.setOnClickListener(
+      new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+          String email = inputEmail.getText().toString().trim();
+          String password = inputPassword.getText().toString().trim();
 
-                if (TextUtils.isEmpty(password)) {
-                    Toast.makeText(getApplicationContext(), "Por favor, insira uma senha!", Toast.LENGTH_SHORT).show();
-                    return;
-                }
+          if (TextUtils.isEmpty(email)) {
+            Toast
+              .makeText(
+                getApplicationContext(),
+                "Por favor, insira um email!",
+                Toast.LENGTH_SHORT
+              )
+              .show();
+            return;
+          }
 
-                if (password.length() < 6) {
-                    Toast.makeText(getApplicationContext(), "A senha é muito curta, deve ter pelo menos 6 caracteres!", Toast.LENGTH_SHORT).show();
-                    return;
-                }
+          if (TextUtils.isEmpty(password)) {
+            Toast
+              .makeText(
+                getApplicationContext(),
+                "Por favor, insira uma senha!",
+                Toast.LENGTH_SHORT
+              )
+              .show();
+            return;
+          }
 
-                createAccount(email, password);
-            }
-        });
+          if (password.length() < 6) {
+            Toast
+              .makeText(
+                getApplicationContext(),
+                "A senha é muito curta, deve ter pelo menos 6 caracteres!",
+                Toast.LENGTH_SHORT
+              )
+              .show();
+            return;
+          }
 
-        buttonLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish(); // Opcional: encerrar a atividade atual para remover da pilha de atividades
-            }
-        });
-    }
+          createAccount(email, password);
+        }
+      }
+    );
+  }
 
-    private void createAccount(String email, String password) {
-        auth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, task -> {
-                    if (task.isSuccessful()) {
-                        Toast.makeText(RegisterActivity.this, "Registro bem-sucedido!", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-                        startActivity(intent);
-                        finish();
-                    } else {
-                        Toast.makeText(RegisterActivity.this, "Registro falhou! Por favor, tente novamente.", Toast.LENGTH_SHORT).show();
-                    }
-                });
-    }
+  // Criando conta
+  private void createAccount(String email, String password) {
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .addOnCompleteListener(
+        this,
+        task -> {
+          if (task.isSuccessful()) {
+            Toast
+              .makeText(
+                RegisterActivity.this,
+                "Registro bem-sucedido!",
+                Toast.LENGTH_SHORT
+              )
+              .show();
+            Intent intent = new Intent(
+              RegisterActivity.this,
+              LoginActivity.class
+            );
+            startActivity(intent);
+            finish();
+          } else {
+            Toast
+              .makeText(
+                RegisterActivity.this,
+                "Registro falhou! Por favor, tente novamente.",
+                Toast.LENGTH_SHORT
+              )
+              .show();
+          }
+        }
+      );
+  }
 }
