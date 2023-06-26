d3f11a2fdda563a605417ec8df93f8c9df8cfff0
Henrique <hp_bonfim@hotmail.com>
2023-06-25 22:48:51 -0400

    lint code

--- app/src/main/java/com/trabalho_final_progmov/podcastz/activities/LoginActivity.java
+++ app/src/main/java/com/trabalho_final_progmov/podcastz/activities/LoginActivity.java
@@ -1,4 +1,5 @@
 package com.trabalho_final_progmov.podcastz.activities;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -14,66 +15,101 @@ import com.trabalho_final_progmov.podcastz.R;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private FirebaseAuth auth;
-    private EditText inputEmail, inputPassword;
-    private Button buttonLogin, buttonCreateUser;
+  private FirebaseAuth auth;
+  private EditText inputEmail, inputPassword;
+  private Button buttonLogin, buttonCreateUser;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_login);
 
-        // Iniciando Firebase Auth
-        FirebaseApp.initializeApp(this);
-        auth = FirebaseAuth.getInstance();
+    // Iniciando Firebase Auth
+    FirebaseApp.initializeApp(this);
+    auth = FirebaseAuth.getInstance();
 
-        inputEmail = findViewById(R.id.input_username);
-        inputPassword = findViewById(R.id.input_password);
-        buttonLogin = findViewById(R.id.button_login);
-        buttonCreateUser = findViewById(R.id.button_create_user);
+    inputEmail = findViewById(R.id.input_username);
+    inputPassword = findViewById(R.id.input_password);
+    buttonLogin = findViewById(R.id.button_login);
+    buttonCreateUser = findViewById(R.id.button_create_user);
 
-        buttonLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = inputEmail.getText().toString().trim();
-                String password = inputPassword.getText().toString().trim();
+    buttonLogin.setOnClickListener(
+      new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+          String email = inputEmail.getText().toString().trim();
+          String password = inputPassword.getText().toString().trim();
 
-                if (TextUtils.isEmpty(email)) {
-                    Toast.makeText(getApplicationContext(), "Por favor, insira um email!", Toast.LENGTH_SHORT).show();
-                    return;
-                }
+          if (TextUtils.isEmpty(email)) {
+            Toast
+              .makeText(
+                getApplicationContext(),
+                "Por favor, insira um email!",
+                Toast.LENGTH_SHORT
+              )
+              .show();
+            return;
+          }
 
-                if (TextUtils.isEmpty(password)) {
-                    Toast.makeText(getApplicationContext(), "Por favor, insira uma senha!", Toast.LENGTH_SHORT).show();
-                    return;
-                }
+          if (TextUtils.isEmpty(password)) {
+            Toast
+              .makeText(
+                getApplicationContext(),
+                "Por favor, insira uma senha!",
+                Toast.LENGTH_SHORT
+              )
+              .show();
+            return;
+          }
 
-                signIn(email, password);
-            }
-        });
+          signIn(email, password);
+        }
+      }
+    );
 
-        buttonCreateUser.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-    }
+    buttonCreateUser.setOnClickListener(
+      new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+          Intent intent = new Intent(
+            LoginActivity.this,
+            RegisterActivity.class
+          );
+          startActivity(intent);
+          finish();
+        }
+      }
+    );
+  }
 
-    private void signIn(String email, String password) {
-        auth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, task -> {
-                    if (task.isSuccessful()) {
-                        Toast.makeText(LoginActivity.this, "Login realizado com sucesso!", Toast.LENGTH_SHORT).show();
-                        FirebaseUser user = auth.getCurrentUser();
-                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                        startActivity(intent);
-                        finish();
-                    } else {
-                        Toast.makeText(LoginActivity.this, "Falha no login! Por favor, tente novamente.", Toast.LENGTH_SHORT).show();
-                    }
-                });
-    }
+  private void signIn(String email, String password) {
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .addOnCompleteListener(
+        this,
+        task -> {
+          if (task.isSuccessful()) {
+            Toast
+              .makeText(
+                LoginActivity.this,
+                "Login realizado com sucesso!",
+                Toast.LENGTH_SHORT
+              )
+              .show();
+            FirebaseUser user = auth.getCurrentUser();
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+          } else {
+            Toast
+              .makeText(
+                LoginActivity.this,
+                "Falha no login! Por favor, tente novamente.",
+                Toast.LENGTH_SHORT
+              )
+              .show();
+          }
+        }
+      );
+  }
 }
--- app/src/main/java/com/trabalho_final_progmov/podcastz/entities/PlaylistResponse.java
+++ app/src/main/java/com/trabalho_final_progmov/podcastz/entities/PlaylistResponse.java
@@ -3,13 +3,14 @@ package com.trabalho_final_progmov.podcastz.entities;
 import java.util.List;
 
 public class PlaylistResponse {
-    private boolean status;
-    private List<PlaylistItem> items;
-    private int count;
-    private String max;
-    private String description;
 
-    public List<PlaylistItem> getItems() {
-        return items;
-    }
+  private boolean status;
+  private List<PlaylistItem> items;
+  private int count;
+  private String max;
+  private String description;
+
+  public List<PlaylistItem> getItems() {
+    return items;
+  }
 }
